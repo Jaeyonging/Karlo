decd5f615bd661edb8d315f63c331aefee7321ce
jaeyonging <wodyd1318@naver.com>
2024-06-20 15:59:06 +0900

    add: ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Suspense, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import { Draw } from "./routes/Draw";
 import { Footer } from "./components/Footer";
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = import.meta.env.VITE_GA_PROPERTYID;
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.send('pageview');
+  }, [location]);
   return (
     <>
       <Suspense fallback={<div>로딩중</div>}>
